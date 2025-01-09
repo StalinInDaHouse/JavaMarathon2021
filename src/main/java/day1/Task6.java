c61c1bc6a2314eacd8ad6aafc70956e4657cb856
Pikalov Dmitriy <72594243+StalinInDaHouse@users.noreply.github.com>
2025-01-09 23:46:44 +0700

    First day homework

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,7 +1,14 @@
 package day1;
 
+import java.util.Scanner;
+
 public class Task6 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        for (int i=1;i<10;i++){
+            int result = i*k;
+            System.out.printf("%d X %d = %d\n",i,k,result);
+        }
     }
 }
